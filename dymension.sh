e4dcc20bb91fa851e1b87da155368e3cf1bf5990
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-08-22 22:35:25 +0300

    Update dymension.sh

--- dymension.sh
+++ dymension.sh
@@ -64,8 +64,7 @@ sudo rm -rf /usr/local/go && \
 sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
 rm "go$ver.linux-amd64.tar.gz" && \
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
-source $HOME/.bash_profile && \
-go version
+source $HOME/.bash_profile
 fi
 
 # download binary
